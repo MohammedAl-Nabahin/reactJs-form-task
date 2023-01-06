c2db567430c7081cfc08c1b50e1aba9e358a16ee
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-01-06 23:43:30 +0200

    Adding Main Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
 import Rigister from '../src/Pages/Rigister';
-import LogIn from '../src/Pages/LogIn';
+import LogIn from '../src/Pages/LogIn'
+import { Routes, Route } from 'react-router-dom';
+import Home from '../src/Pages/Home'
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
   state = {
-    showComponent1: true
+    showComponent1: true,
+ 
   };
 
   goToLogIn= () =>{
-      this.setState({showComponent1:false});
+      this.setState({showComponent1:true});
   }
 
   goToRigister=()=>{
+    this.setState({showComponent1:false});
+  }
+
+  goToHome=()=>{
     this.setState({showComponent1:true});
   }
 
 
+
+
   render() {
     return (
-        this.state.showComponent1 ? <Rigister switch={this.goToLogIn} /> : <LogIn goToRigister={this.goToRigister}/>
-    )
+        this.state.showComponent1 ?   <Home/> : <LogIn goToRigister={this.goToRigister} goToHome={this.goToHome}/> 
+        )
   }
 }
